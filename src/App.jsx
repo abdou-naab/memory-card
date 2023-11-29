44a6e836a24081acdff3f0b7c2b32b5bfa0bc47f
abdou <113465542+abdou-naab@users.noreply.github.com>
2023-11-29 20:15:11 +0100

    add video bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import bgVideo from "./assets/bg.mp4";
 function App() {
-  return <></>;
+  return (
+    <>
+      <video id="bg-video" src={bgVideo} autoPlay loop muted></video>
+    </>
+  );
 }
 
 export default App;
